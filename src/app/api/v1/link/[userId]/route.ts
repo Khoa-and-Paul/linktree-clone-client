89ff9bad766716116108aaa57e22e123ebeb5210
Khoa Nguyen <henrykhoanguyen@gmail.com>
2024-01-15 14:45:02 -0600

    change alias project directories to module path

--- src/app/api/v1/link/[userId]/route.ts
+++ src/app/api/v1/link/[userId]/route.ts
@@ -1,27 +1,27 @@
 import { NextRequest, NextResponse } from "next/server";
-import prisma from "../../../../../../lib/prisma";
+import prisma from "@/lib/prisma";
 
 // Grab user's links based off param userId passed in
 export async function GET(
-    request: NextRequest,
-    { params } : { params: {userId: string}}
+  request: NextRequest,
+  { params }: { params: { userId: string } }
 ) {
-    try {
-        if (!params.userId) {
-            return new NextResponse("User id is required", { status: 400 });
-        }
-        const { userId } = params;
-        const links = await prisma.links.findMany({
-            where: {
-                userId: Number(userId)
-            }
-        });
-        if (links) {
-            return NextResponse.json(links);
-        } else {
-            return NextResponse.json({}, {status: 404});
-        }
-    } catch(e) {
-        return NextResponse.json(e, {status: 500});
+  try {
+    if (!params.userId) {
+      return new NextResponse("User id is required", { status: 400 });
     }
+    const { userId } = params;
+    const links = await prisma.links.findMany({
+      where: {
+        userId: Number(userId),
+      },
+    });
+    if (links) {
+      return NextResponse.json(links);
+    } else {
+      return NextResponse.json({}, { status: 404 });
+    }
+  } catch (e) {
+    return NextResponse.json(e, { status: 500 });
+  }
 }
--- src/app/api/v1/user/[id]/route.ts
+++ src/app/api/v1/user/[id]/route.ts
@@ -1,27 +1,27 @@
 import { NextRequest, NextResponse } from "next/server";
-import prisma from "../../../../../../lib/prisma";
+import prisma from "@/lib/prisma";
 
 // Grab a user's information based on a id passed in the param
 export async function GET(
-    request: NextRequest,
-    { params } : { params: {id: string}}
+  request: NextRequest,
+  { params }: { params: { id: string } }
 ) {
-    try {
-        if (!params.id) {
-            return new NextResponse("User id is required", { status: 400 });
-        }
-        const { id } = params;
-        const user = await prisma.users.findUnique({
-            where: {
-                id: Number(id)
-            }
-        });
-        if (user) {
-            return NextResponse.json(user);
-        } else {
-            return NextResponse.json({}, {status: 404});
-        }
-    } catch(e) {
-        return NextResponse.json(e, {status: 500});
+  try {
+    if (!params.id) {
+      return new NextResponse("User id is required", { status: 400 });
     }
+    const { id } = params;
+    const user = await prisma.users.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+    if (user) {
+      return NextResponse.json(user);
+    } else {
+      return NextResponse.json({}, { status: 404 });
+    }
+  } catch (e) {
+    return NextResponse.json(e, { status: 500 });
+  }
 }
